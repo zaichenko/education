28a9d7bec8780fc39c29c0ec5d1df5368a6d0fed
migewoo <pyannikov_74@mal.ru>
2025-04-10 13:55:03 +0300

    cs

--- test.cs
+++ test.cs
@@ -31,3 +31,29 @@ abracadabra
 restart project...
 please, ne zasoryaite file!
 >>>>>>> a84b17b5a9733e8d062b206f00aeb8e25a71bcf0
+using System;
+					
+public class Program	
+{
+	static int g;
+	
+	public static void Main()
+	{
+	Fox test = new Fox();
+	test.Eat();
+		test.Run(10);
+		Console.WriteLine();
+	}
+}
+public class Fox
+{
+	public void Run(int distance)
+{
+	Console.WriteLine("fox running " + distance + " km");
+}
+public void Eat()
+{
+Console.WriteLine("fox eating");
+}
+
+}
