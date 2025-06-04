c576403d2627d1e895b8280d2ae434797911a687
Andrey <7045943a@gmail.com>
2025-06-04 19:52:45 +0300

    all tests

--- Fox.cs
+++ Fox.cs
@@ -11,18 +11,18 @@ namespace education
         public string Run(int distance)
         {
             switch (distance)
-            {
-                case <= 0:
-                    return "fox do not running";
-                    break;
-                case <= 10:
-                    return "fox running " + distance + " km";
-                    break;
-                default:
-                    return "fox do not running";
-                    break;
+                {
+                    case <= 0:
+                        return "fox do not running";
+                        break;
+                    case <= 10:
+                        return "fox running " + distance + " km";
+                        break;
+                    default:
+                        return "fox do not running";
+                        break;
 
-            }
+                }
             return "";
         }
         public void Eat()
--- FoxTests.cs
+++ FoxTests.cs
@@ -6,8 +6,42 @@ public class FoxTests
     [Test]
     public void Run_NegativeDistance_NotRuning()
     {
+        int distance = -10;
         Fox testing1 = new Fox();
-        string output = testing1.Run(-10);
-        ClassicAssert.AreEqual(output, "fox do not running");
+        string output = testing1.Run(distance);
+        ClassicAssert.AreEqual("fox do not running", output);
+    }
+    [Test]
+    public void Run_MaxDistance_Runnig()
+    {
+        int distance = 10;
+        Fox testing1 = new Fox();
+        string output = testing1 .Run(distance);
+        ClassicAssert.AreEqual("fox running " + distance + " km", output);
+    }
+    [Test]
+    public void Run_BigDistance_NotRuning()
+    {
+        int distance = 100;
+        Fox testing1 = new Fox();
+        string output = testing1.Run(distance);
+        ClassicAssert.AreEqual("fox do not running", output);
+    }
+    [Test]
+    public void Run_ZeroDistance_NotRunnig()
+    {
+        int distance = 0;
+        Fox testing1 = new Fox();
+        string output = testing1.Run(distance);
+        ClassicAssert.AreEqual("fox do not running", output);
+    }
+    [Test]
+
+    public void Run_NormalDistance_Running()
+    {
+        int distance = 4;
+        Fox testing1 = new Fox();
+        string output = testing1.Run(distance);
+        ClassicAssert.AreEqual("fox running " + distance + " km", output);
     }
 }
